ff68368b3ed1544a30f0196dfa16dd5747f60e70
Manikandan Jaishanker <56948634+mjaishanker@users.noreply.github.com>
2021-05-19 13:08:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,17 +16,17 @@ provider "azurerm" {
 
 
 
-resource "azurerm_resource_group" "rg" {
+/*resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
-}
+}*/
 
 resource "azurerm_resource_group" "rg2" {
   name     = "myTFResourceGroup2"
-  location = "eastus2"
+  location = "centralus"
 }
 
 resource "azurerm_resource_group" "rg3" {
   name     = "myTFResourceGroup3"
-  location = "eastus2"
+  location = "westus2"
 }
